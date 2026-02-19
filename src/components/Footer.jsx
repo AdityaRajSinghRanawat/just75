3fd1b85667aee195c9f31535eb2d4972594a2a1d
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2026-02-19 15:56:26 +0530

    footer update: emoji text updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full shrink-0 h-14 md:h-auto py-2 md:py-4 text-center text-xs text-slate-700 bg-white border-t border-slate-200">
-      <div>made with love ❤️ by</div>
+      <div>made with ❤️ by</div>
       <div>Aditya Raj Singh Ranawat</div>
     </footer>
   );

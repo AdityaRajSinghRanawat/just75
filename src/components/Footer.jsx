29dab5651f55fc411881c74a681f6d948ff2dec2
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2026-02-20 14:28:18 +0530

    made hear pulse animation in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="w-full shrink-0 h-14 md:h-auto py-2 md:py-4 text-center text-xs text-slate-700 bg-white border-t border-slate-200">
-      <div>made with ❤️ by</div>
+      <div>
+        made with <span className="inline-block animate-pulse">{"\u2764\uFE0F"}</span> by
+      </div>
       <div>Aditya Raj Singh Ranawat</div>
     </footer>
   );
